4321db2fe5b84743eee7f3a00026b020e8400fa6
Tanner Richards <Tanner.richards990@gmail.com>
2022-08-05 10:46:03 -0700

    messed with css properties to setup page the way I intended

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 // import functions and grab DOM elements
+const defeatedGruntsEl = document.getElementById('defeated-grunts');
+const characterHpEl = document.getElementById('character-hp');
+const gruntContainerEl = document.getElementById('grunt-area');
+const formEl = document.querySelector('form');
 
 // let state
+let characterHP = 10;
+let defeatedGrunts = 0;
+const gruntList = [
+    {
+        name: 'Monty',
+        hitPoints: 3
+    },
+    {
+        name: 'Juicer',
+        hitpoints: 5
+    },
+    {
+        name: 'Tito',
+        hitpoints: 10,
+    }
+];
 
 // set event listeners 
   // get user input
